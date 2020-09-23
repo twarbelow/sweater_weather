e019c1b054a5ea9661ebacb1d743c8d8707d9a03
twarbelow <taija.warbelow@gmail.com>
2020-09-23 13:10:32 -0700

    trails service

---
+++ app/services/trails_service.rb
@@ -0,0 +1,20 @@
+class TrailsService
+  def self.get_trails(lat, long)
+    response = conn.get("data/get-trails?lat=#{lat}&lon=#{long}&maxDistance=10")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private
+
+  def self.conn
+    Faraday.new(
+      url: 'https://www.hikingproject.com',
+      params: {
+        key: ENV['HIKING_DATA_API_KEY']
+      },
+      headers: {
+        'Content-Type' => 'application/json'
+      }
+    )
+  end
+end
