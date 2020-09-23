8f197f42be1d761c15d050e0d9643a3e7c8c2b26
twarbelow <taija.warbelow@gmail.com>
2020-09-23 13:50:04 -0700

    more arguments line 4

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -1,7 +1,8 @@
 class TrailsController < ApplicationController
   def index
     geo = GeoFacade.get_coords(params[:location])
-    trails = TrailsFacade.get_trails(geo.lat, geo.long)
+    trails = TrailsFacade.get_trails(geo.lat, geo.long, params[:location])
+    render json: trails
       # array of trail poros
   end
 end
