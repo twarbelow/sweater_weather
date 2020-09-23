30c660c838e6af741871a29373d4c485dde1052f
twarbelow <taija.warbelow@gmail.com>
2020-09-23 07:34:45 -0700

    init for weatherporo

--- app/poros/weather_poro.rb
+++ app/poros/weather_poro.rb
@@ -1,9 +1,9 @@
 class WeatherPoro
-  attr_reader
+  attr_reader :current, :hourly, :daily
   def initialize(info)
-    @current =
-    @hourly =
-    @daily =
+    @current = info[:current]
+    @hourly = info[:hourly].first(8)
+    @daily = info[:daily].first(5)
   end
 
 end
