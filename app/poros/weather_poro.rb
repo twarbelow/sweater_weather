22c0f8f47ebe9b9717342db8abba970c15f1b493
twarbelow <taija.warbelow@gmail.com>
2020-09-23 07:52:31 -0700

    init methods for proper keys

--- app/poros/weather_poro.rb
+++ app/poros/weather_poro.rb
@@ -1,9 +1,12 @@
 class WeatherPoro
-  attr_reader :current, :hourly, :daily
+  attr_reader :current, :hourly, :daily, :current_keys, :hourly_keys, :daily_keys
   def initialize(info)
     @current = info[:current]
     @hourly = info[:hourly].first(8)
     @daily = info[:daily].first(5)
+    @current_keys = [:dt, :sunrise, :sunset, :temp, :feels_like, :humidity, :uvi, :weather]
+    @hourly_keys = [:dt, :temp, :weather]
+    @daily_keys = [:dt, :sunrise, :sunset, :rain, :temp, :humidity, :weather]
+      # in temp for daily, only get :min and :max
   end
-
 end
