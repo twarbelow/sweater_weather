0465fbdadf28c322c94cc2be3c58f3ea8b1e2127
twarbelow <taija.warbelow@gmail.com>
2020-09-23 13:00:50 -0700

    preliminary index method

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -2,6 +2,6 @@ class TrailsController < ApplicationController
   def index
     geo = GeoFacade.get_coords(params[:location])
     trails = TrailsFacade.get_trails(geo.lat, geo.long)
-
+      # array of trail poros
   end
 end
