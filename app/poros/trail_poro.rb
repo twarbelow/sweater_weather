b0f145c403f0818b96785ca1b69109d3bce03788
twarbelow <taija.warbelow@gmail.com>
2020-09-23 13:52:11 -0700

    distance_to_trail now in info hash

--- app/poros/trail_poro.rb
+++ app/poros/trail_poro.rb
@@ -1,12 +1,11 @@
 class TrailPoro
-  attr_reader :name, :summary, :difficulty, :location
-  attr_accessor :distance_to_trail
+  attr_reader :name, :summary, :difficulty, :location, :distance_to_trail
 
   def initialize(info)
     @name = info[:name]
     @summary = info[:summary]
     @difficulty = info[:difficulty]
     @location = info[:location]
-    @distance_to_trail = nil
+    @distance_to_trail = info[:distance_to_trail]
   end
 end
