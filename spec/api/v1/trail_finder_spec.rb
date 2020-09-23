e29290c038437212adcae7163b3b8bc81b2e5465
twarbelow <taija.warbelow@gmail.com>
2020-09-23 14:01:11 -0700

    fix typos

--- spec/api/v1/trail_finder_spec.rb
+++ spec/api/v1/trail_finder_spec.rb
@@ -14,20 +14,15 @@ RSpec.describe "trails endpoint" do
 
     expect(json[:data][:type]).to eq("trail")
     expect(json[:data][:attributes][:location]).to eq(location)
-    expect(json[:data][:attributes][:forecast][:summary].class).to eq(String)
-    expect(json[:data][:attributes][:forecast][:temperature].class).to eq(String)
+    # expect(json[:data][:attributes][:forecast][:summary].class).to eq(String)
+    # expect(json[:data][:attributes][:forecast][:temperature].class).to eq(String)
 
-    jason[:trails].each do |trail|
-      expect(trails).to include(:name)
-      expect(trails).to include(:summary)
-      expect(trails).to include(:difficulty)
-      expect(trails).to include(:location)
-      expect(trails).to include(:distance_to_trail)
+    json[:data][:attributes][:trails].each do |trail|
+      expect(trail).to include(:name)
+      expect(trail).to include(:summary)
+      expect(trail).to include(:difficulty)
+      expect(trail).to include(:location)
+      expect(trail).to include(:distance_to_trail)
     end
   end
 end
-
-#will need temp and summary from weather
-#will need name summary difficulty and location from trails
-#will use each trail location and original location to get distance_to_trail
-  # https://www.mapquestapi.com/directions/v2/route?key=KEY&from=Denver%2C+CO&to=Boulder%2C+CO&outFormat=json&ambiguities=ignore&routeType=fastest&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false
