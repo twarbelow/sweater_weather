7b2add383414c6e5efe233360fa29a867114eb2f
twarbelow <taija.warbelow@gmail.com>
2020-09-23 13:15:18 -0700

    trailporo, no distance_to_trail

---
+++ app/poros/trail_poro.rb
@@ -0,0 +1,11 @@
+class TrailPoro
+  attr_reader :name, :summary, :difficulty, :location, :distance_to_trail
+
+  def initialize(info)
+    @name = info[:name]
+    @summary = info[:summary]
+    @difficulty = info[:difficulty]
+    @location = info[:location]
+    @distance_to_trail = nil
+  end
+end
