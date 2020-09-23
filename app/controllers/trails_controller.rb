274281d09b24fe5aa3b1d5c187becafa59695696
twarbelow <taija.warbelow@gmail.com>
2020-09-23 14:13:07 -0700

    assign forecast variable

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -2,7 +2,17 @@ class TrailsController < ApplicationController
   def index
     geo = GeoFacade.get_coords(params[:location])
     trails = TrailsFacade.get_trails(geo.lat, geo.long, params[:location])
-    render json: trails
-      # array of trail poros
+    forecast = ForecastFacade.get_forecast(geo.lat, geo.long)
+    render json: {
+      "data": {
+        "id": "null",
+        "type": "trail",
+        "attributes": {
+          "location": params[:location],
+          "forecast": forecast,
+          "trails": trails
+        }
+      }
+    }
   end
 end
