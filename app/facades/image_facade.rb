a57de9a33ba926de504ea039dfc1474a9341dbe5
twarbelow <taija.warbelow@gmail.com>
2020-09-23 21:35:58 -0700

    add location as argument)

--- app/facades/image_facade.rb
+++ app/facades/image_facade.rb
@@ -1,7 +1,6 @@
 class ImageFacade
   def self.get_image(location)
     response = ImageService.search_images(location)
-    require 'pry'; binding.pry
     ImagePoro.new(response, location)
   end
 end
