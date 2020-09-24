43591169865d89acafcbfb54ad815882ec12f86c
twarbelow <taija.warbelow@gmail.com>
2020-09-24 11:22:12 -0700

    geo service cassette, geo service spec passing

---
+++ spec/api/v1/services/geo_service.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'GeoService' do
+  it 'returns geo service response when provided with location' do
+    VCR.use_cassette('geo_service', :match_requests_on => [:method, :path], :record => :new_episodes) do
+      location = "denver, co"
+      returned = GeoService.geo_location(location)
+      expect(returned).to be_a(Hash)
+      expect(returned).to include(:results)
+      expect(returned[:results]).to be_a(Array)
+      expect(returned[:results][0]).to include(:locations)
+      expect(returned[:results][0][:locations]).to be_a(Array)
+      expect(returned[:results][0][:locations][0]).to include(:latLng)
+      expect(returned[:results][0][:locations][0][:latLng]).to be_a(Hash)
+      expect(returned[:results][0][:locations][0][:latLng]).to include(:lat)
+      expect(returned[:results][0][:locations][0][:latLng][:lat]).to be_a(Float)
+      expect(returned[:results][0][:locations][0][:latLng]).to include(:lng)
+      expect(returned[:results][0][:locations][0][:latLng][:lng]).to be_a(Float)
+
+    end
+  end
+end
