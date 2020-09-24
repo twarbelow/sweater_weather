60cf768d9a7e2dfd131c7bf17239d68bb2be2d9e
twarbelow <taija.warbelow@gmail.com>
2020-09-23 21:36:39 -0700

    correct structure, spec passing

--- spec/api/v1/requests/background_spec.rb
+++ spec/api/v1/requests/background_spec.rb
@@ -9,13 +9,12 @@ RSpec.describe "GET /api/v1/backgrounds" do
     json = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to have_http_status(200)
-
     expect(json[:data][:type]).to eq("image")
-    expect(json[:data][:id]).to eq(null)
-    expect(json[:data][:image][:location]).to eq(location)
-    expect(json[:data][:image][:image_url].class).to eq(String)
-    expect(json[:data][:image][:credit][:source]).to eq("pixabay.com")
-    expect(json[:data][:image][:credit][:author].class).to eq(String)
-    expect(json[:data][:image][:credit][:logo]).to eq("https://pixabay.com/static/img/logo_square.png")
+    expect(json[:data][:attributes][:id]).to eq("null")
+    expect(json[:data][:attributes][:image][:location]).to eq(location)
+    expect(json[:data][:attributes][:image][:image_url].class).to eq(String)
+    expect(json[:data][:attributes][:image][:credit][:source]).to eq("pixabay.com")
+    expect(json[:data][:attributes][:image][:credit][:author].class).to eq(String)
+    expect(json[:data][:attributes][:image][:credit][:logo]).to eq("https://pixabay.com/static/img/logo_square.png")
   end
 end
