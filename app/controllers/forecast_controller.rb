cb5ac74a84a5a3c9b4dde8e22dd80210382933f9
twarbelow <taija.warbelow@gmail.com>
2020-09-24 09:06:20 -0700

    add user of serializer

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -2,6 +2,6 @@ class ForecastController < ApplicationController
   def show
     geo = GeoFacade.get_coords(params[:location])
     weather = WeatherFacade.get_weather(geo.lat, geo.long)
-    render json: weather
+    render json: ForecastSerializer.new(weather)
   end
 end
