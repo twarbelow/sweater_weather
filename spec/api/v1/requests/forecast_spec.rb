dab56dbc917497016eb798c638c1bcca232dd0ca
twarbelow <taija.warbelow@gmail.com>
2020-09-24 13:39:39 -0700

    weather poro spec passing

--- spec/api/v1/requests/forecast_spec.rb
+++ spec/api/v1/requests/forecast_spec.rb
@@ -2,17 +2,18 @@ require 'rails_helper'
 
 RSpec.describe "show action" do
   it "responds to a get request with properly formatted weather data for the correct location" do
-    location = "denver, co"
-    headers = { "ACCEPT" => "application/json",
-                "CONTENT_TYPE" => "application/json" }
-    get "/api/v1/forecast?location=#{location}", headers: headers
+    VCR.use_cassette('forecast_spec', :match_requests_on => [:method, :path], :record => :new_episodes) do
 
-    json = JSON.parse(response.body, symbolize_names: true)
+      location = "denver, co"
+      headers = { "ACCEPT" => "application/json",
+                  "CONTENT_TYPE" => "application/json" }
+      get "/api/v1/forecast?location=#{location}", headers: headers
 
-    expect(json.keys).to include(:hourly)
-    expect(json.keys).to include(:daily)
-    expect(json.keys).to include(:current)
+      json = JSON.parse(response.body, symbolize_names: true)
 
-
+      expect(json.keys).to include(:hourly)
+      expect(json.keys).to include(:daily)
+      expect(json.keys).to include(:current)
+    end
   end
 end
