c73fa3dec2ecb54432485dc8d3200e5d25df12ef
twarbelow <taija.warbelow@gmail.com>
2020-09-24 12:16:01 -0700

    comment out unnecessary expectations

--- spec/api/v1/services/geo_service.rb
+++ spec/api/v1/services/geo_service.rb
@@ -6,16 +6,17 @@ RSpec.describe 'GeoService' do
       location = "denver, co"
       returned = GeoService.geo_location(location)
       expect(returned).to be_a(Hash)
-      expect(returned).to include(:results)
       expect(returned[:results]).to be_a(Array)
-      expect(returned[:results][0]).to include(:locations)
       expect(returned[:results][0][:locations]).to be_a(Array)
-      expect(returned[:results][0][:locations][0]).to include(:latLng)
       expect(returned[:results][0][:locations][0][:latLng]).to be_a(Hash)
-      expect(returned[:results][0][:locations][0][:latLng]).to include(:lat)
       expect(returned[:results][0][:locations][0][:latLng][:lat]).to be_a(Float)
-      expect(returned[:results][0][:locations][0][:latLng]).to include(:lng)
       expect(returned[:results][0][:locations][0][:latLng][:lng]).to be_a(Float)
+      
+      # expect(returned).to include(:results)
+      # expect(returned[:results][0]).to include(:locations)
+      # expect(returned[:results][0][:locations][0]).to include(:latLng)
+      # expect(returned[:results][0][:locations][0][:latLng]).to include(:lat)
+      # expect(returned[:results][0][:locations][0][:latLng]).to include(:lng)
     end
   end
 end
