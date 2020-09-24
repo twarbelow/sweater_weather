9ca9454aee5d0f30b6d370438c8fac088ec78edf
twarbelow <taija.warbelow@gmail.com>
2020-09-24 10:52:30 -0700

    image_service cassette, image service spec passing

---
+++ spec/api/v1/services/image_service.rb
@@ -0,0 +1,15 @@
+
+require 'rails_helper'
+
+RSpec.describe 'ImageService' do
+  it 'returns image service when provided with a serach term' do
+    VCR.use_cassette('image_service, :match_requests_on => [:method, :path], :record => :new_episodes) do
+      location = "denver, co"
+      returned = ImageService.search_images(location)
+      expect(returned).to be_a(Hash)
+      expect(returned[:hits][0]).to include(:largeImageURL)
+      expect(returned[:hits][0]).to include(:user)
+      expect(returned[:hits][0]).to include(:type => "photo")
+    end
+  end
+end
